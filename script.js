83fdcd758d7facb145121489cfa823c36d9ebd9f
Cassy Pearson <seepearson@gmail.com>
2020-03-21 17:07:45 -0700

    styling and weather API key

---
+++ script.js
@@ -0,0 +1,21 @@
+var APIkey = "3cac1a7dde76b023ab18654db33900cb";
+var queryURL= "http://api.openweathermap.org/data/2.5/forecast?id=524901&apiid=" + APIkey;
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function (response) {
+      console.log(queryURL);
+      console.log(response);
+      
+      $(".date").text("Today: " + response.dt_txt);
+      $(".city").html("<h1>" + response.name + " Today's Weather</h1>");
+      $(".wind").text("Wind Speed: " + response.wind.speed);
+      $(".temp").text("Temperature (K) " + response.main.temp);
+      // $(".uv").text("UV: " + response.main.humidity);
+      // $(".forcast").text("forecast: " + response.);
+      // $(".conditions").text("Conditons: " + response.id)
+      // $(".icon").text("" + response.icon);; 
+    });
+   
