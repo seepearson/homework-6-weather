9afdf0036f0887af4572c48735c7bfa9a35191b7
Cassy Pearson <seepearson@gmail.com>
2020-03-26 17:11:05 -0700

    Tucson weather is working

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-var APIkey = "3cac1a7dde76b023ab18654db33900cb";
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + "Tucson,Arizona&appid=" + APIkey;
 
-$(function () {
-    $("#navbar-search").click(function () {
-        if ($("#navbar-search").val == "Search our website") {
-            $('#navbar-search').val("");
-        }
-    })
+  $("#button").on("click", function(){
+      event.preventDefault();
+      
+  })
+ var APIKey = "166a433c57516f51dfab1f7edaed8413"; 
+ var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+  "q=Tucson,Arizona&appid=" + APIKey;
+  
+$.ajax({
+  url: queryURL,
+  method: "GET"
+})
+
+  .then(function(response) {
+    console.log(queryURL);
+    console.log(response);
 
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        .then(function (response) {
-            console.log(queryURL);
-            console.log(response);
+    $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+    $(".wind").text("Wind Speed: " + response.wind.speed);
+    $(".humidity").text("Humidity: " + response.main.humidity);
+    $(".icon").text("" + response.weather[0].icon);
 
-            $(".date").text("Today: " + response.dt_txt);
-            $(".city").html("<h1>" + response.name + " Today's Weather</h1>");
-            $(".wind").text("Wind Speed: " + response.wind.speed);
-            $(".temp").text("Temperature (K) " + response.main.temp);
-            $(".uv").text("UV: " + response.main.humidity);
-            $(".forcast").text("forecast: " + response);
-            $(".conditions").text("Conditons: " + response.id)
-            $(".icon").text("" + response.icon);;
-        });
 
     var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
 
     $(".temp").text("Temperature (K) " + response.main.temp);
     $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
 
+
     console.log("Wind Speed: " + response.wind.speed);
     console.log("Humidity: " + response.main.humidity);
     console.log("Temperature (F): " + tempF);
+    console.log(response.weather[0].icon)
+  });
